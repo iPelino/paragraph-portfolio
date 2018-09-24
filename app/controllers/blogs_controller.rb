a286a5817ade3668c8074d5a678d99ffda7ca48c
pelino <pelmut2000@gmail.com>
2018-09-24 18:24:24 +0200

    Updated status for enum

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -64,6 +64,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
+      #implementing Friendly_ID
       @blog = Blog.friendly.find(params[:id])
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_124805) do
+ActiveRecord::Schema.define(version: 2018_09_24_161218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_09_24_124805) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
