bb9872f3195f640d04ad319e71f776c0966b6723
pelino <pelmut2000@gmail.com>
2018-09-24 19:03:46 +0200

    Implemented toggle status on blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -59,6 +59,19 @@ class BlogsController < ApplicationController
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  #for toggle_status
+  def toggle_status
+    #byebug  'for debugging purposes'
+    #
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published? 
+      @blog.draft!
+    end
+
+    redirect_to blogs_url, notice: 'Blog status has been updated.'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact' , to: 'pages#contact'
-  resources :blogs
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   root to: 'pages#home'
 
